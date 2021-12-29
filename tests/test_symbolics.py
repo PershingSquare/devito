7c83e8fbe152ddd7b855ce19bfe5b7927ed59ef1
George Bisbas <g.bisbas18@imperial.ac.uk>
2021-12-29 17:16:22 +0200

    compiler: split to reduce and evaluate funcs
    wip-rebase

--- tests/test_symbolics.py
+++ tests/test_symbolics.py
@@ -291,7 +291,7 @@ class TestRelationsWithAssumptions(object):
 
     def test_multibounds_op(self):
         """
-        Tests evalrel function on a simple example.
+        Tests evaluate_relation function on a simple example.
         """
 
         grid = Grid(shape=(16, 16, 16))
@@ -309,7 +309,7 @@ class TestRelationsWithAssumptions(object):
         f = TimeFunction(name='f', grid=grid, space_order=2)
 
         f.data[:] = 0.1
-        eqns = [Eq(f.forward, f.laplace + f * evalrel(min, [f, b, c, d]))]
+        eqns = [Eq(f.forward, f.laplace + f * evaluate_relation(min, [f, b, c, d]))]
         op = Operator(eqns, opt=('advanced'))
         op.apply(time_M=5)
         fnorm = norm(f)
@@ -318,7 +318,8 @@ class TestRelationsWithAssumptions(object):
         d2 = Function(name='d2', grid=grid)
 
         f.data[:] = 0.1
-        eqns = [Eq(f.forward, f.laplace + f * evalrel(min, [f, b, c2, d2], [Ge(d, c)]))]
+        eqns = [Eq(f.forward, f.laplace + f * evaluate_relation(min, [f, b, c2, d2],
+                [Ge(d, c)]))]
         op = Operator(eqns, opt=('advanced'))
         op.apply(time_M=5)
         fnorm2 = norm(f)
@@ -358,7 +359,7 @@ class TestRelationsWithAssumptions(object):
         eqn = eval(expr)
         assumptions = eval(assumptions)
         expected = eval(expected)
-        assert evalrel(op, eqn, assumptions) == expected
+        assert evaluate_relation(op, eqn, assumptions) == expected
 
     @pytest.mark.parametrize('op, expr, assumptions, expected', [
         ([min, '[a, b, c, d]', '[Ge(b, a), Ge(a, b), Le(c, b), Le(b, d)]', 'c']),
@@ -401,7 +402,7 @@ class TestRelationsWithAssumptions(object):
         eqn = eval(expr)
         assumptions = eval(assumptions)
         expected = eval(expected)
-        assert evalrel(op, eqn, assumptions) == expected
+        assert evaluate_relation(op, eqn, assumptions) == expected
 
     @pytest.mark.parametrize('op, expr, assumptions, expected', [
         ([min, '[a, b, c, d]', '[Ge(b, a)]', 'a']),
@@ -424,4 +425,4 @@ class TestRelationsWithAssumptions(object):
         eqn = eval(expr)
         assumptions = eval(assumptions)
         expected = eval(expected)
-        assert evalrel(op, eqn, assumptions) == expected
+        assert evaluate_relation(op, eqn, assumptions) == expected
